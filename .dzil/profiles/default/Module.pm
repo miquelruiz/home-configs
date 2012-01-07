e45005d5c933505b146caa6eb0d43f61f1e458c3
Miquel Ruiz <mruiz@cpan.org>
2012-01-07 18:55:13 +0100

    Initial commit
    Configs for: bash, Dist::Zilla, git, screen, vim, and my public keys

---
+++ .dzil/profiles/default/Module.pm
@@ -0,0 +1,35 @@
+package {{$name}};
+
+use strict;
+use warnings;
+
+
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+=over 4
+
+=item *
+Miquel Ruiz <mruiz@cpan.org>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Miquel Ruiz.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
